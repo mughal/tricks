d628638ab90325ba8df91f7e046ee8a2477f9c83
mughal <mughal@gmail.com>
2024-06-23 11:45:16 +0500

    library all books and views

--- express-locallibrary-tutorial/routes/catalog.js
+++ express-locallibrary-tutorial/routes/catalog.js
@@ -18,7 +18,7 @@ router.get("/book/:id", book_controller.book_detail);
 router.get("/books", book_controller.book_list);
 
 // Author Routes 
-router.get("/author/create", author_controller.author_create_get);book
+router.get("/author/create", author_controller.author_create_get);
 router.post("/author/create", author_controller.author_create_post);
 router.get("/author/:id/delete", author_controller.author_delete_get);
 router.post("/author/:id/delete", author_controller.author_delete_post);
@@ -36,3 +36,15 @@ router.get("/genre/:id/update", genre_controller.genre_update_get);
 router.post("/genre/:id/update", genre_controller.genre_update_post);
 router.get("/genre/:id", genre_controller.genre_detail);
 router.get("/genres", genre_controller.genre_list);
+
+// BookInstance  Routes 
+router.get("/bookinstance/create", book_instance_controller.bookinstance_create_get);
+router.post("/bookinstance/create", book_instance_controller.bookinstance_create_post);
+router.get("/bookinstance/:id/delete", book_instance_controller.bookinstance_delete_get);
+router.post("/bookinstance/:id/delete", book_instance_controller.bookinstance_delete_post);
+router.get("/bookinstance/:id/update", book_instance_controller.bookinstance_update_get);
+router.post("/bookinstance/:id/update", book_instance_controller.bookinstance_update_post);
+router.get("/bookinstance/:id", book_instance_controller.bookinstance_detail);
+router.get("/bookinstances", book_instance_controller.bookinstance_list);
+
+module.exports = router;
