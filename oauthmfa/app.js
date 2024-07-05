4dbde2a6bc4e9308fea143c3bd9ba8e5c1a1bcc3
mughal <yasir.mirza@sngpl.com.pk>
2024-07-05 12:46:04 +0500

    Moving on with authentication

--- oauthmfa/app.js
+++ oauthmfa/app.js
@@ -4,6 +4,9 @@ console.log(process.env.GOOGLE_CLIENT_ID);
 console.log(process.env.GOOGLE_CLIENT_SECRET);
 console.log(process.env.GOOGLE_CALLBACK_URL);
 console.log(process.env.SESSION_SECRET);
+console.log(process.env.AD_URL);
+console.log(process.env.AD_BASE_DN);
+console.log(process.env.AD_USERNAME);
 const express = require('express');
 const session = require('express-session');
 require('./auth');
@@ -20,11 +23,16 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.set('view engine', 'ejs');
 
 
 
 app.get('/', (req, res) => {
     res.send('<a href="/auth/google"> Auth with Google </a>');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
 });
 
 app.get('/auth/google',
@@ -43,11 +51,16 @@ app.get('/auth/failure', (req, res) => {
 });
 
 app.get('/logout', (req, res) => {
-    res.logout();
-    req.session.destroy();
-    res.send('Goodbye');
+    req.logout((err) => {
+        if (err) { return next(err); }
+        req.session.destroy((err) => {
+            if (err) {
+                return next(err);
+            }
+            res.send('Goodbye');
+        });
+    });
 });
-
 
 app.get('/protected', isLoggedIn, (req, res) => {
     res.send(`Hello  ${req.user.displayName}` );
--- oauthmfa/auth.js
+++ oauthmfa/auth.js
@@ -1,5 +1,36 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
+const ActiveDirectory = require('activedirectory');
+
+const adConfig = {
+  url: process.env.AD_URL,
+  baseDN: process.env.AD_BASE_DN,
+  username: process.env.AD_USERNAME,
+  password: process.env.AD_PASSWORD
+};
+const ad = new ActiveDirectory(adConfig);
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    ad.authenticate(username, password, function(err, auth) {
+      if (err) {
+        return done(err);
+      }
+      if (!auth) {
+        return done(null, false, { message: 'Incorrect username or password.' });
+      }
+      // Fetch user info from AD and pass it to done
+      ad.findUser(username, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+        return done(null, user);
+      });
+    });
+  }
+));
+
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
